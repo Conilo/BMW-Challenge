bea8a4129bf236dd2d1b8f9d959c0d4db8aa0807
Cesar Bravo <conilo@gmail.com>
2018-04-09 16:23:34 -0500

    Update BluetoothCommNode.py

--- src/nodes/BluetoothComm/BluetoothCommNode.py
+++ src/nodes/BluetoothComm/BluetoothCommNode.py
@@ -4,7 +4,7 @@ import bluetooth as bt
 import os
 from bmw.msg import Sensors
 
-# MAC Addresses
+# Dictionary with the known MAC Addresses
 MAC = {'48:3C:0C:9C:AE:02': 'Conilo',
        '5C:51:81:11:7F:F0': 'Pariente',
        '5C:51:88:50:F5:52': 'San Java',
@@ -13,19 +13,24 @@ MAC = {'48:3C:0C:9C:AE:02': 'Conilo',
 # Open server socket on RFCOMM
 server_socket = bt.BluetoothSocket(bt.RFCOMM)
 
-# Server socket parameters
+# Set server socket parameters
 port = 1
 server_socket.bind(("", port))
 server_socket.listen(1)
 
+# Send each new data message by bluetooth
 def on_new_sensors_msg(data):
-
+    """
+    This fucntion sends every new data package 
+    received to the bluetooth comm port.
+    """
     global client_socket
-
-    # Get some data
+       
     try:
-        print("Waitting request...")
+       
+        # Waits for the app request 
         receivedData = client_socket.recv(1024)
+       
         # Concatenate data to be sent
         sensors_data = \
             " " + "D=" + str(data.distance) + "+" \
@@ -36,7 +41,6 @@ def on_new_sensors_msg(data):
 
         # Send data
         client_socket.send(sensors_data)
-        print("   Data sent: %s" % sensors_data)
 
     except:
         os.system("rosnode kill BluetoothComm")
@@ -48,10 +52,12 @@ def main():
 
     rospy.init_node('BluetoothComm')
 
+    # Establishes connection to the ext. device.
     print("Waitting for connection...")
     client_socket, address = server_socket.accept()
     print(" Accepted connection from: " + MAC[address[0]])
 
+    # Subscribes to the sensor's data topic.
     rospy.Subscriber(
         '/sensors_data',
         Sensors,
