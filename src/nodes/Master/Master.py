ef5c474f14e4c4d46443697f0e0e3fbe2df1f3bb
Cesar Bravo <conilo@gmail.com>
2018-04-09 17:39:10 -0500

    Update Master.py

--- src/nodes/Master/Master.py
+++ src/nodes/Master/Master.py
@@ -10,25 +10,34 @@ from master_module import Master, \
 master = Master()
 
 def start_mapping():
+    """
+    Function that sets the mapping flag
+    to start mapping.
+    """
     master.mapping_flag = True
-    tm.sleep(0.2)
 
 def my_shutdown():
+    """
+    Functions that shutsdown motors
+    and clean GPIOs.
+    """
     bw.shutdown()
 
+# Iterrupt object declaration
 bw.board.Enable_Interrupt_Button(1, start_mapping)
 
-# Global parameters
-
 def on_new_sensors_msg(data):
+    """
+    Function that saves the sensor's data into
+    the master object.
+    """
     master.distance = data.distance
     master.voltage = data.voltage
     master.current = data.current
-    #master.left_sensor_value = data.color_values[0]
-    #master.right_sensor_value = data.color_values[1]
-    master.left_sensor_string = data.color_strings[0]
-    master.right_sensor_string = data.color_strings[1]
+    master.left_sensor_value = data.color_values[0]
+    master.right_sensor_value = data.color_values[1]
 
+    # Runs the task assigner and solver
     master.run()
 
 def main():
