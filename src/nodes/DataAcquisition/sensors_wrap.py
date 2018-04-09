1b38d81145e049aa7a64a487c652a032d73e599b
Cesar Bravo <conilo@gmail.com>
2018-04-09 16:31:38 -0500

    Update sensors_wrap.py

--- src/nodes/DataAcquisition/sensors_wrap.py
+++ src/nodes/DataAcquisition/sensors_wrap.py
@@ -13,19 +13,38 @@ left_color_sensor = color.colorbk(2)
 right_color_sensor = color.colorbk(1)
 
 def read_ultrasonic():
-    tm.sleep(0.01)
+    """
+    Reads the ultrasonic sensor using 
+    bmw provided libs.
+    """
     return ultrasonic_sensor.read()
 
 def read_battery_voltage():
+    """
+    Reads the power sensor voltage value 
+    using bmw provided libs.
+    """
     return power_sensor.readvoltage()
 
 def read_drained_current():
+    """
+    Reads the power sensor current value 
+    using bmw privided libs.
+    """
     return power_sensor.readcurrent()
 
 def read_color_values():
+    """
+    Reads the light sensor strings using 
+    bmw privided libs.
+    """
     return(right_color_sensor.read("white"),
            left_color_sensor.read("white"))
 
 def read_color_strings():
+    """
+    Reads the light sensor values using 
+    bmw privided libs.
+    """
     return(right_color_sensor.read(),
            left_color_sensor.read())
