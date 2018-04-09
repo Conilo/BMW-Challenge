134bf694655c028f384742dd11565b2aabb4e832
Cesar Bravo <conilo@gmail.com>
2018-04-09 16:35:54 -0500

    Update bmw_wrap.py

--- src/nodes/Master/bmw_wrap.py
+++ src/nodes/Master/bmw_wrap.py
@@ -12,16 +12,26 @@ board.Enable_Led(1)
 board.Enable_Led(2)
 board.Enable_Led(3)
 
+# Initialize motor objects
 left_motor = mt.motorbk(7)
 right_motor = mt.motorbk(6)
 
+
 def shutdown():
+    """
+    Function that takes care to properly
+    close and clean GPIOs and motors.
+    """
     left_motor.set(0)
     right_motor.set(0)
     board.CleanUp()
     print('GPIOs down')
 
 def set_motors_speeds(vel_L, vel_R):
+    """
+    Funtion that sets the linear 
+    velocities of both motors.
+    """
     left_motor.set(vel_L)
     right_motor.set(vel_R)
-    tm.sleep(0.2)
+    tm.sleep(0.1)
